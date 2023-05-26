d739741d1df727aa5861885d640845051c54041c
Elizabeth Brooks <merimaat1@gmail.com>
2023-05-26 13:22:28 -0400

    Added initial scripts

---
+++ formattingCustomDatabases_assignTaxonomy.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#$ -M ebrooks5@nd.edu
+#$ -m abe
+#$ -r n
+#$ -N taxonAssign_jobOutput
+
+# creator: Elizabeth Brooks
+# e-mail: ebrooks5@nd.edu
+# date: 26 May 2023
+
+# BASH script to parse CO1_.fasta and CO1_taxonomy.txt and create 
+# a fasta file with the sequence ID replaced with the 
+# taxonomic assignment (column 2) from CO1_taxonomy.txt
+# https://benjjneb.github.io/dada2/training.html
+# https://ucedna.com/reference-databases-for-metabarcoding 
+
+# usage: qsub formattingCustomDatabases_assignTaxonomy.sh workingDir subsetTag
+
+# set working directory with input files
+workingDir=$1
+
+# change to the working directory
+cd $workingDir
+
+# set the subset tag for the split fasta file
+subsetTag=$2
+
+# set output fasta file name
+outRef="CO1_taxonomyAssignment.fasta."$subsetTag
+
+# set temporary file names
+tmpRef="tmp_CO1_.fasta."$subsetTag
+tmpDB="tmp_CO1_taxonomy.txt."$subsetTag
+
+# make a coppy of the temporary CO1_taxonomy.txt file for the current subset
+# in order to avoid conflicts between the scripts running on each subset
+cp "tmp_CO1_taxonomy.txt" $tmpDB
+
+# make sure a previous output fasta does not exsist
+rm $outRef
+
+# loop over each line (sequence) in the temporary singleline fasta
+while IFS= read -r line; do
+	# retrieve the current sequence ID
+	# and remove the > tag
+	seqID=$(echo $line | cut -d '@' -f1 | sed 's/>//g')
+	# output a status message
+	echo "Processing $seqID ..."
+	# open the CO1_taxonomy.txt file and search for the current ID word,
+	# retrieve the taxonomy assignment,
+	# add the > tag, and append to the output fasta
+	cat $tmpDB | grep -w "$seqID" | cut -f2 | sed 's/^/>/g' >> $outRef
+	# retrieve the current sequence, replace the ^ tags with newlines,
+	# and append to the output fasta
+	echo $line | cut -d '@' -f2 >> $outRef
+done < $tmpRef
+
+# remove empty lines from the output fasta
+sed -i.bak '/^$/d' $outRef
+
+# clean up and remove temporary files
+rm $tmpRef
+rm $tmpDB
+
+# output a status message
+echo "Analysis complete!"
---
+++ formattingCustomDatabases_driver.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# creator: Elizabeth Brooks
+# e-mail: ebrooks5@nd.edu
+# date: 26 May 2023
+
+# BASH script to split the input fasta file and run a separate BASH script to
+# parse CO1_.fasta and CO1_taxonomy.txt and create 
+# a fasta file with the sequence ID replaced with the 
+# taxonomic assignment (column 2) from CO1_taxonomy.txt
+# https://benjjneb.github.io/dada2/training.html
+# https://ucedna.com/reference-databases-for-metabarcoding 
+
+# usage: bash formattingCustomDatabases_driver.sh
+
+# set working directory with input files
+workingDir="/Users/bamflappy/Downloads"
+
+# change to the working directory
+cd $workingDir
+
+# set input file names
+inRef="CO1_.fasta"
+inDB="CO1_taxonomy.txt"
+
+# set output fasta file name
+outRef="CO1_taxonomyAssignment.fasta"
+
+# set temporary file names
+tmpRef="tmp_CO1_.fasta"
+tmpDB="tmp_CO1_taxonomy.txt"
+
+# prepare a temporary CO1_taxonomy.txt file by replacing white spaces with tabs
+# since the third column of taxonomic assignment is space separated
+cat $inDB | tr ' ' '\t' > $tmpDB
+
+# create temporary singleline fasta by 
+# opening the CO1_.fasta file, add a ^ tag to the end of each line, 
+# remove all new lines, and add newlines back just before each sequence >
+cat $inRef | sed -e 's/$/@/' | tr -d '\n' | sed 's/>/\n>/g' > $tmpRef
+
+# split the temporary singleline fasta into smaller subsets of sequences
+split -l 1000 $tmpRef $tmpRef"."
+
+# loop over each segment
+for i in $tmpRef"."*; do
+	# retrieve subset tag
+	subsetTag=$(basename $i | sed 's/CO1\_\.fasta\.//g')
+	# output status message
+	echo "Starting analysis for subset $subsetTag ..."
+	# generate Ka and Ks values for protein sequences
+	qsub -N "taxonAssign_"$subsetTag formattingCustomDatabases_assignTaxonomy.sh $workingDir $subsetTag
+done
+
+# wait for the jobs to finish running
+# using the current working directory
+qsub -hold_jid "taxonAssign_"* -cwd
+
+# combine the subsets of fasta files
+cat $tmpRef"."* > $outRef
+
+# output a status message
+echo "Analysis complete!"
